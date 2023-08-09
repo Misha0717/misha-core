aebb812a4a0f6352bffd82ce6b7d34d840ce24b8
Misha <misha.oldenburger@gmail.com>
2023-08-09 03:17:45 +0200

    small things

--- [core]/misha/client/main.lua
+++ [core]/misha/client/main.lua
@@ -5,3 +5,9 @@ end)
 RegisterCommand('test', function(source, args, RawCommand)
     TriggerEvent("playerSpawned")
 end)
+
+RegisterNetEvent("misha:client:player:spawned", function(player, NewPlayer)
+    if source == "" then
+        return -- prevent mod menus
+    end
+end)
